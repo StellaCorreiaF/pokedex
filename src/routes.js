269439a0149e740851060da9aab2913ca626a2cf
StellaCorreiaF <stellafernanda@icloud.com>
2022-08-23 21:09:20 -0300

    define rotas para recuperar legendary

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const legendaryController = require('./app/controllers/LegendaryController');
 const routes = new Router(); 
 
 routes.get('/legendaries', legendaryController.index); 
+routes.get('/legendaries/name', legendaryController.listData);
 
 module.exports = routes; 
