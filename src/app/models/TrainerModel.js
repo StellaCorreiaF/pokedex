143b868b28dc12d7fbc0d9501ec2f3ac692c80bc
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-06 17:46:48 -0300

    refatora model de trainer

--- src/app/models/TrainerModel.js
+++ src/app/models/TrainerModel.js
@@ -1,8 +1,27 @@
-function trainerModel(id, name, email, password){
+import Sequelize, { Model } from 'sequelize'
+class TrainerModel extends Model {
+    static init(sequelize) {
+        super.init({
+            id: Sequelize.UUIDV4(),
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password: Sequelize.STRING
+
+        }, {
+           sequelize 
+        })
+
+    }
+}
+
+/*function trainerModel(id, name, email, password){
     this.id= id; 
     this.name = name;
     this.email = email; 
     this.password = password; 
 }
 
-module.exports = trainerModel; 
+module.exports = trainerModel; */
+
+export { TrainerModel }
+
