9d17b49e55eb209c9d0d134c698a84dd6aa66d60
StellaCorreiaF <stellafernanda@icloud.com>
2022-09-06 21:22:03 -0300

    altera rotas

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,40 @@
 //const Router= require('express');  - Refatorando, import
 import Router from 'express';
 import multer from 'multer';
+import CreateLegendaryController from './app/controllers/Legendary/CreateLegendaryController';
+import DeleteLegendaryController from './app/controllers/Legendary/DeleteLegendaryController';
+import ListLegendaryController from './app/controllers/Legendary/ListLegendaryController';
+import UpdateLegendaryController from './app/controllers/Legendary/UpdateLegendaryController';
+import CreateTrainerController from './app/controllers/Trainer/CreateTrainerController';
+import ListAllTrainnersController from './app/controllers/Trainer/ListAllTrainersController';
+import SessionController from './app/controllers/Trainer/SessionController';
+import UpdateTrainerController from './app/controllers/Trainer/UpdateTrainerController';
 import multerConfig from './config/multer';
 import legendaryValidator from './middlewares/LegendaryValidator';
 
 
-const legendaryController = require('./app/controllers/Legendary/LegendaryController');
-const trainerController = require('./app/controllers/Trainer/TrainerController');
+
 const updloadFileController = require('./app/controllers/UploadFileController');
-const sessionController = require('./app/controllers/Trainer/SessionController');
 const uploadFile = multer({ storage : multerConfig})
 
 
 const routes = new Router(); 
 
-routes.get('/legendaries', legendaryController.index); 
-routes.get('/legendaries/name', legendaryController.listData);
-routes.post('/legendaries', legendaryValidator, legendaryController.create); //o middleware tem q ser alocado sempre como segundo parametro da rota
-routes.put('/legendaries/:id',legendaryController.update);
-routes.delete('/legendaries/:id', legendaryController.delete);
+routes.get('/legendaries', ListLegendaryController.index); 
+routes.get('/legendaries/name', ListLegendaryController.listData);
+routes.post('/legendaries', legendaryValidator, CreateLegendaryController.create); //o middleware tem q ser alocado sempre como segundo parametro da rota
+routes.put('/legendaries/:id',UpdateLegendaryController.update);
+routes.delete('/legendaries/:id', DeleteLegendaryController.delete);
 
 
 
+routes.get('/trainer/name', ListAllTrainnersController.listAll);
+routes.post('/trainer', CreateTrainerController.create); 
+routes.put('/trainer/:id',UpdateTrainerController.update);
 
-/*routes.get('/trainer', trainerController.index); 
-routes.get('/trainer/name', trainerController.listData);
-routes.post('/trainer', trainerController.create); 
-routes.put('/trainer/:id',trainerController.update);
-routes.delete('/trainer/:id', trainerController.delete);*/
-routes.post('/session', sessionController.create );
+
+
+routes.post('/session', SessionController.create );
 
 
 
